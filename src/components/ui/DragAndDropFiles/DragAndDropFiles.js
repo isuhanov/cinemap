68e547da8dc52f0ed62b80b044ba05809b4b8ea4
tuc-nado <signat1911@gmail.com>
2022-08-15 00:30:48 +0300

    Сделал отмены добавления файла в DragAndDrop

--- src/components/ui/DragAndDropFiles/DragAndDropFiles.js
+++ src/components/ui/DragAndDropFiles/DragAndDropFiles.js
@@ -1,66 +1,72 @@
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useState } from "react";
 
 import './DragAndDropFiles.css';
 
 const DragAndDropFiles = memo(({ photoList, onDropFiles }) => {
     const [drag, setDrag] = useState(false);
     const [fileList, setFileList] = useState(photoList);
-    const dropRef = useRef();
-    
-    useEffect(() => {
-        let dragCounter;
-        function handleDrag(e) {
-            e.preventDefault()
-            e.stopPropagation()
-        }
-        function handleDragIn(e) {
-            e.preventDefault()
-            e.stopPropagation()
-            dragCounter++
-            if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-                setDrag(true)
-            }
-        }
-        function handleDragOut(e) {
-            e.preventDefault()
-            e.stopPropagation()
-            dragCounter--
-            if (dragCounter === 0) {
-                setDrag(false);
-            }
-        }
-        function handleDrop(e) {
-            e.preventDefault()
-            e.stopPropagation()
-            setDrag(false);
-            if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-                let files = fileList;
-                for (const file of e.dataTransfer.files) {
-                    files.push(file);
-                }
-                setFileList(files);
-                onDropFiles(files);
-                dragCounter = 0    
-            }
-        }
 
-        let div = dropRef.current;
-        div.addEventListener('dragenter', handleDragIn)
-        div.addEventListener('dragleave', handleDragOut)
-        div.addEventListener('dragover', handleDrag)
-        div.addEventListener('drop', handleDrop)
-    }, []);
+    let dragCounter;
+    function handleDrag(e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+    function handleDragIn(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        dragCounter++
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+            setDrag(true)
+        }
+    }
+    function handleDragOut(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        dragCounter--
+        if (dragCounter === 0) {
+            setDrag(false);
+        }
+    }
+    function handleDrop(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        setDrag(false);
+        if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+            let files = fileList;
+            for (const file of e.dataTransfer.files) {
+                files.push(file);
+            }
+            setFileList(files);
+            onDropFiles(files);
+            dragCounter = 0    
+        }
+    }
+   
+    function onRemoveFiles(index) {
+        let files = fileList;
+        files.splice(index, 1);
+        setFileList(files);
+        onDropFiles(files);
+    }
 
     return (
-        <div ref={dropRef} className="drag-and-drop-container">
+        <div onDragEnter={handleDragIn} onDragLeave={handleDragOut} onDragOver={handleDrag} onDrop={handleDrop} 
+            className="drag-and-drop-container">
             <div className="drag-and-drop-filelist-container">
                 { photoList.length > 0 ?
                     <ul className="drag-and-drop-filelist">
                         { photoList.map((file, index) => {
-                            return <li className="drag-and-drop-file" key={index}>
-                                <span className="material-symbols-outlined">image</span>
-                                <span>{ file.name }</span>
+                            return (
+                                <li className="drag-and-drop-file" key={index}>
+                                    <button onClick={() => onRemoveFiles(index)} type="button" className="file-remove-btn">
+                                        <span className="material-symbols-outlined">
+                                            remove
+                                        </span>
+                                    </button>
+                                    <span className="material-symbols-outlined">image</span>
+                                    <span>{ file.name }</span>
                                 </li>
+                            )
                         }) }
                     </ul>
                     :
