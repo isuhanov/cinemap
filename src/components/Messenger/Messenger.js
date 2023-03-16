9e9dd85d2ea04629179c35d261a06516fc1d2ce4
tuc-nado <signat1911@gmail.com>
2023-03-16 19:31:31 +0300

    Переключание режимов месенджера

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -75,11 +75,11 @@ const Messenger = memo(({ onClickClose, otherClassName, onReload, otherUserId, o
                         Мессенджер
                     </p>
                     <div className="header-btn-container">
-                        <button className="header-btn" onClick={() => {
-                                                            setMode('users');
-                                                        }}
-                        >
-                            <span className="material-symbols-outlined">add_box</span>
+                        <button className={`${mode === 'chats' ? 'btn-is-active' : 'btn-is-disabled'} header-btn `} onClick={() => setMode('chats')}>
+                            <span className="material-symbols-outlined">chat_bubble</span>
+                        </button>
+                        <button className={`${mode === 'users' ? 'btn-is-active': 'btn-is-disabled'} header-btn `} onClick={() => setMode('users')}>
+                            <span className="material-symbols-outlined">person</span>
                         </button>
                         <button className="header-btn" onClick={onClickClose}>
                             <span className="material-symbols-outlined">close</span>
